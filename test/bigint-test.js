0f3cde67f5d9df9ff080d71c314d29491dbfaa35
nicholasmueller <nicholasmueller91@gmail.com>
2019-12-12 13:49:56 +0700

    fix

--- test/bigint-test.js
+++ test/bigint-test.js
@@ -1,7 +1,7 @@
 var mocha  = require('mocha')
   , assert = require('chai').assert
   , expect = require('chai').expect
-  , BigNumber = require('bignumber.js')
+  , BigNumber = require('bn.js')
   ;
 
 describe("Testing bigint support", function(){
